51f26cd1a2e48713abd1b5e1d7e6f775cae3e35c
shyamalaram <41112375+shyamalaram@users.noreply.github.com>
2018-07-15 14:28:05 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+package demo;
+import java.util.Scanner;
+public class hello {
+public static void main(String args[]){
+	int i,n;
+	System.out.println("enter n:");
+	Scanner tt=new Scanner(System.in);
+	n =tt.nextInt();
+	for(i=1;i<=n;i++){
+		System.out.println("hello");
+	}
+}
+}
