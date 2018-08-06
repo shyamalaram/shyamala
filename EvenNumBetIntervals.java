22a5ac76e8b2907a7ede1b73e939f368b748121f
shyamalaram <41112375+shyamalaram@users.noreply.github.com>
2018-08-06 12:03:16 +0530

    Create EvenNumBetIntervals.java

---
+++ EvenNumBetIntervals.java
@@ -0,0 +1,24 @@
+package guvigithub;
+
+import java.util.Scanner;
+
+public class evenNumberBetIntervals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sn=0;
+		int en=0;
+		System.out.println("enter the Starting number:");
+		System.out.println("enter the ending number");
+		Scanner in=new Scanner(System.in);
+		sn=in.nextInt();
+		en=in.nextInt();
+		for(int i=sn;i<=en;i++){
+			while(i%2==0){
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+}
