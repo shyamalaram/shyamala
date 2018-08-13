71001c4a9744e124b1dd7b91385daa37a780cff5
shyamalaram <41112375+shyamalaram@users.noreply.github.com>
2018-08-13 12:19:50 +0530

    Create CountNoWordsInString.java

---
+++ CountNoWordsInString.java
@@ -0,0 +1,18 @@
+package guvigithub;
+import java .util.Scanner;
+public class CountingTheWordInString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="";
+int i;
+Scanner input=new Scanner(System.in);
+System.out.println("enter the string:");
+str=input.nextLine();
+String s[]=str.split(" ");	
+for(i=0;i<s.length;i++){
+	int m=i+i;
+}
+System.out.println(i);
+}
+}
